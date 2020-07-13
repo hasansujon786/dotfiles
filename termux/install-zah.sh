0b93e1336e4478c97d976fa1f4d6de502d7c969c
hasansujon786 <hasansujon786@gmail.com>
2020-07-13 23:22:05 +0600

    Update termux/install-zah.sh

--- termux/install-zah.sh
+++ termux/install-zah.sh
@@ -2,4 +2,5 @@
 termux-setup-storage
 apt update && apt upgrade
 sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+printf 'source ~/dotfiles/bash/autocomplete.zsh' >> ~/.zshrc
 
