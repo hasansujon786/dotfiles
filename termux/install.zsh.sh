2ea0a7842eb6b59f92c2dde43f9d7fa7a50ddf22
hasansujon786 <hasansujon786@gmail.com>
2020-08-11 17:18:12 +0600

    Update termux config

--- termux/install.zsh.sh
+++ termux/install.zsh.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-termux-setup-storage
-apt update && apt upgrade
-sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+sh -c "$(curl -fsSL https://github.com/hasansujon786/termux-ohmyzsh/raw/master/install.sh)"
 printf 'source ~/dotfiles/bash/autocomplete.zsh' >> ~/.zshrc
 rm -Rf /data/data/com.termux/files/usr/etc/motd
-printf 'Welcome to Termux!' >> /data/data/com.termux/files/usr/etc/motd
+echo 'Welcome to Termux!' >> /data/data/com.termux/files/usr/etc/motd
