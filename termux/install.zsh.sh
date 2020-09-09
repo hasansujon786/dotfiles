b7b3f5104ce4c049dcccbb8094fa46d01e9cc987
hasansujon786 <hasansujon786@gmail.com>
2020-09-09 19:39:58 +0600

    delete cascadia from install.zsh

--- termux/install.zsh.sh
+++ termux/install.zsh.sh
@@ -3,4 +3,3 @@ sh -c "$(curl -fsSL https://github.com/hasansujon786/termux-ohmyzsh/raw/master/i
 printf 'source ~/dotfiles/bash/autocomplete.zsh' >> ~/.zshrc
 rm -Rf /data/data/com.termux/files/usr/etc/motd
 echo 'Welcome to Termux!' >> /data/data/com.termux/files/usr/etc/motd
-cp ~/dotfiles/termux/cascadia.ttf ~/.termux/font.ttf
