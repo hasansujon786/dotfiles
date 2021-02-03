7189c3ca0a8f45b493a7710ae1e3f54fcc56a32a
hasansujon786 <hasansujon786@gmail.com>
2021-02-03 16:02:41 +0600

    feat: add bash util to find org file's contents

--- bash/utils.zsh
+++ bash/utils.zsh
@@ -10,3 +10,16 @@ zle -N fg-bg
 bindkey '^z' fg-bg
 bindkey '^x^x' edit-command-line
 
+# mkdir and cd in one command
+mcd() {
+  mkdir -p -- "$1" &&
+    cd -P -- "$1" || return
+}
+
+fog() { ${EDITOR:-vim} $(rg -n '.*' "$HOME/Documents/org" | fzf --prompt "fnote> " --height 50% --ansi | sed -E 's/(.*):([0-9]+):.*/\1 +\2/g'); }
+# notegrep() { ${EDITOR:-vi} -c "NGrep $*"; }
+# command! -nargs=0 NGrep grep! ".*" ~/Documents/org/**/*.org
+
+# Calculator
+# calc() { echo "scale=5;$*" | bc -l; }
+
