290019351489835d793699b7a518485d02e5d463
hasansujon786 <hasansujon786@gmail.com>
2021-08-15 20:07:57 +0000

    script: add elementary-os

---
+++ scripts/elementary-os.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##!/bin/sh
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:philip.scott/pantheon-tweaks
+sudo apt install -y pantheon-tweaks
+
+sudo add-apt-repository multiverse -y
+sudo apt install ubuntu-restricted-extras -y
+
+# sudo apt install gnome-tweaks -y
+
--- scripts/pop-os.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-##!/bin/sh
-
-sudo add-apt-repository multiverse -y
-sudo apt install ubuntu-restricted-extras -y
-
-sudo apt install gnome-tweaks -y
-
