80e3c6f4b0fc45039c94bf19964e10cde4d4fb1b
hasansujon786 <hasansujon786@gmail.com>
2021-08-16 22:18:13 +0600

    scritp: add gnome-disk-utility to elementary-os

--- scripts/elementary-os.sh
+++ scripts/elementary-os.sh
@@ -4,8 +4,10 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:philip.scott/pantheon-tweaks
 sudo apt install -y pantheon-tweaks
 
-sudo add-apt-repository multiverse -y
-sudo apt install ubuntu-restricted-extras -y
+sudo add-apt-repository -y multiverse
+sudo apt install -y ubuntu-restricted-extras
+
+sudo apt-get install -y gnome-disk-utility
 
 # sudo apt install gnome-tweaks -y
 
