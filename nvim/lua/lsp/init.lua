936782cb3ead552d44c2dc4dc1507ebf248e409c
hasansujon786 <hasansujon786@gmail.com>
2021-09-03 16:55:52 +0600

    feat(lsp): add border to help popup window

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,5 +1,8 @@
 local M = {}
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'double' })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'double' })
+
 vim.fn.sign_define(
   "LspDiagnosticsSignError",
   { texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError" }
@@ -20,16 +23,13 @@ vim.fn.sign_define(
 local function lsp_document_highlight(client)
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
+    vim.api.nvim_exec([[
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]],
-      false
-    )
+      ]], false)
   end
 end
 
