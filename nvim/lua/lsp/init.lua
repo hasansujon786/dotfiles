137b29797930101a37d4d9ee2e1d2b57dde07926
hasansujon786 <hasansujon786@gmail.com>
2021-10-09 13:25:28 +0600

    fix: fix diagnostic breaking changes 5.1

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -9,9 +9,9 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagn
   update_in_insert = false,
 })
 
-local signs = { Error = '', Warning = '', Hint = '', Information = '' }
+local signs = { Error = '', Warn = '', Hint = '', Info = '' }
 for type, icon in pairs(signs) do
-  local hl = 'LspDiagnosticsSign' .. type
+  local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
