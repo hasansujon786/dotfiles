0a46a2ac23c9d94f9e94951df4d5b03bfec3f261
hasansujon786 <hasansujon786@gmail.com>
2021-10-10 01:48:32 +0600

    whichkey: update whichkey

--- nvim/lua/config/whichkey.lua
+++ nvim/lua/config/whichkey.lua
@@ -1,9 +1,9 @@
-local wk = require("which-key")
+local wk = require('which-key')
 
 wk.setup {
   window = {
-    border = "none", -- none, single, double, shadow
-    position = "bottom", -- bottom, top
+    border = 'none', -- none, single, double, shadow
+    position = 'bottom', -- bottom, top
     margin = { 0, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
     padding = { 2, 0, 2, 0 }, -- extra window padding [top, right, bottom, left]
   },
@@ -11,7 +11,7 @@ wk.setup {
     height = { min = 4, max = 25 }, -- min and max height of the columns
     width = { min = 20, max = 50 }, -- min and max width of the columns
     spacing = 3, -- spacing between columns
-    align = "left", -- align columns left, center or right
+    align = 'left', -- align columns left, center or right
   },
   key_labels = {
     ['<space>'] = 'SPC',
@@ -24,7 +24,7 @@ wk.setup {
 
 local leader = {
   a = {
-    name = "+lsp", -- optional group name
+    name = '+lsp',
     c = { '<cmd>CocList commands<CR>',                   'commands' },
     e = { '<cmd>CocList extensions<CR>',                 'extensions' },
     o = { '<cmd>CocList outline<CR>',                    'outline' },
@@ -186,6 +186,7 @@ local leader = {
 
   v = {
     name = '+vim',
+    ['/'] = { ':Telescope help_tags<CR>',       'Search Vim help' },
     -- TODO
     -- e = 'print-to-float',
     -- nnoremap <leader>ve :call _#print_to_float(g:)<left>
@@ -251,6 +252,7 @@ local leader = {
     M = { ':Telescope marks<CR>',           'Jump to marks' } ,
     r = { ':Telescope oldfiles<CR>',        'Recent files' },
     t = { ':Telescope filetypes<CR>',       'Change filetypes' },
+    v = { ':Telescope help_tags<CR>',       'Search Vim help' },
     s = { ':lua require("hasan.telescope.custom").grep_string()<CR>',         'Grep string' },
     w = { ':lua require("hasan.telescope.custom").search_wiki_files()<CR>',   'Search wiki files'},
     -- m = { ':Bookmarks<CR>',                 'Jump to bookmark' } ,
