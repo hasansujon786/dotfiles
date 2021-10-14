ea3228567ecbd3831959a1408e4311d6e90e8d9a
hasansujon786 <hasansujon786@gmail.com>
2021-10-14 14:42:44 +0600

    orgmode: update configs

--- nvim/lua/config/orgmode.lua
+++ nvim/lua/config/orgmode.lua
@@ -1,9 +1,9 @@
 require('orgmode').setup({
-  org_agenda_files = {'~/vimwiki/org/**/*'},
-  org_default_notes_file = '~/vimwiki/org/refile.org',
+  org_agenda_files = {'~/vimwiki/**/*'},
+  org_default_notes_file = '~/vimwiki/5_inbox/refile.org',
   org_hide_leading_stars = true,
   org_hide_emphasis_markers = true,
   org_agenda_templates = {
-    m = { description = 'Bookmark', template = '* %?\n  %a', target = '~/vimwiki/3_resources/bookmark.org' },
+    f = { description = 'FileMark', template = '* %?\n  %a', target = '~/vimwiki/5_inbox/file-mark.org' },
   }
 })
