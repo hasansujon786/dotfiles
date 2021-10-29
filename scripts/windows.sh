8fa0350cd6e4006678be280687f67552ada95e0c
hasansujon786 <hasansujon786@gmail.com>
2021-10-29 08:46:25 -0700

    script: add windows.sh

---
+++ scripts/windows.sh
@@ -0,0 +1,18 @@
+choco install git -y
+choco install google-chrome-x64 -y
+choco install sublimetext3 -y
+choco install sublimetext3.packagecontrol -y
+choco install 7zip.install -y
+choco install gimp -y
+choco install vlc -y
+
+# choco install javaruntime -y
+# choco install adobereader -y
+# choco install pdfcreator -y
+# choco install markdownpad2 -y
+# choco install conemu -y
+# choco install mobaxterm -y
+# choco install InkScape -y
+# choco install etcher -y
+# choco install slack -y
+# choco install Xming -y
