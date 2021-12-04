f3c83ed555889119b29d96a3283507b206580c42
hasansujon786 <hasansujon786@gmail.com>
2021-12-04 16:16:21 +0600

    devicon: set new icon

---
+++ nvim/lua/config/devicons-config.lua
@@ -0,0 +1,12 @@
+require("nvim-web-devicons").set_icon {
+  scratchpad = {
+    icon = "",
+    color = "#6d8086",
+    name = "Scratchpad"
+  },
+  NeogitStatus = {
+    icon = "",
+    color = "#F14C28",
+    name = "BranchCycle"
+  },
+}
