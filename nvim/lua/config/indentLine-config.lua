3a8361b34f5fcaa8a5730a4bb9562fafc9ee2017
hasansujon786 <hasansujon786@gmail.com>
2021-12-06 16:48:39 +0600

    ui: update indentLine-config

--- nvim/lua/config/indentLine-config.lua
+++ nvim/lua/config/indentLine-config.lua
@@ -1,7 +1,8 @@
-
 require('indent_blankline').setup {
-  char = '▏',
+  -- char = '▏',
+  -- context_char = '▏',
   buftype_exclude = {'terminal', 'prompt'},
   filetype_exclude = {'help', 'dashboard', 'packer', 'NvimTree', 'Trouble', 'WhichKey' },
-  -- space_char_blankline = ' ',
+  show_first_indent_level = false,
+  -- show_current_context = true,
 }
