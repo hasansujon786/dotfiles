ab47c6e503fc086c81df12d476376b8fbc5d72a7
hasansujon786 <hasansujon786@gmail.com>
2021-12-07 01:44:39 +0600

    fix typo

--- nvim/lua/config/gitgutter-config.lua
+++ nvim/lua/config/gitgutter-config.lua
@@ -2,7 +2,6 @@
 local maps = require('hasan.utils.maps')
 maps.nmap(']c', '<Plug>(GitGutterNextHunk)')
 maps.nmap('[c', '<Plug>(GitGutterPrevHunk)')
-maps.nmap('[c', '<Plug>(GitGutterPrevHunk)')
 maps.nmap('<leader>gp', '<Plug>(GitGutterPreviewHunk)')
 maps.nmap('<leader>gs', '<Plug>(GitGutterStageHunk)')
 maps.nmap('<leader>gr', '<Plug>(GitGutterUndoHunk)')
