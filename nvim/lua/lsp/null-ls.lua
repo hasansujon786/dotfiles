bcc3cce0bb42bbf105f894a8d08bb65bcbeb1416
hasansujon786 <hasansujon786@gmail.com>
2021-12-15 13:01:46 +0600

    null-ls: set prettier

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,2 +1,8 @@
-require('null-ls').config({})
-require('lspconfig')['null-ls'].setup({})
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/MAIN.md
+local null_ls = require("null-ls")
+local b = null_ls.builtins
+local sources = {
+  b.formatting.prettier,
+}
+
+null_ls.setup({ sources = sources })
