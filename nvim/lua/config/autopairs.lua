00368fa21bd450cc273cae0ae1c6243f7ef341ee
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2022-02-06 03:13:08 -0800

    fix: Integrate autopairs with cmp

--- nvim/lua/config/autopairs.lua
+++ nvim/lua/config/autopairs.lua
@@ -1,7 +1,6 @@
 require('nvim-autopairs').setup()
-require('cmp').setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` after select function or method item
-  auto_select = true -- automatically select the first item
-})
+
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
--- nvim/lua/config/indentLine-config.lua
+++ nvim/lua/config/indentLine-config.lua
@@ -1,8 +1,8 @@
 require('indent_blankline').setup {
-  -- char = '▏',
-  -- context_char = '▏',
+  char = '▏',
+  context_char = '▏',
   buftype_exclude = {'terminal', 'prompt'},
   filetype_exclude = {'help', 'dashboard', 'packer', 'NvimTree', 'Trouble', 'WhichKey' },
   show_first_indent_level = false,
-  -- show_current_context = true,
+      -- show_current_context = true,
 }
