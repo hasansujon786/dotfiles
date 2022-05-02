f9876988de097434f8ef7f2aaa2ad767d53072e8
Hasan Mahmdu <hasa>
2022-05-03 01:03:44 +0600

    feat: add lua version of get_visual_selection

--- nvim/lua/hasan/utils/init.lua
+++ nvim/lua/hasan/utils/init.lua
@@ -28,6 +28,35 @@ M.if_nil = function(x, was_nil, was_not_nil)
   end
 end
 
+function M.tbl_length(T)
+  local count = 0
+  for _ in pairs(T) do
+    count = count + 1
+  end
+  return count
+end
+
+function M.tbl_isempty(T)
+  if not T or not next(T) then
+    return true
+  end
+  return false
+end
+
+function M.tbl_concat(...)
+  local result = {}
+  local n = 0
+
+  for _, t in ipairs({ ... }) do
+    for i, v in ipairs(t) do
+      result[n + i] = v
+    end
+    n = n + #t
+  end
+
+  return result
+end
+
 M.is_floting_window = function(winid)
   return vim.api.nvim_win_get_config(winid).relative ~= ''
 end
@@ -283,6 +312,44 @@ function M.normalise_path(path_to_normalise)
   end
 
   return normalised_path
+end
+
+function M.get_visual_selection()
+  -- this will exit visual mode
+  -- use 'gv' to reselect the text
+  local _, csrow, cscol, cerow, cecol
+  local mode = vim.fn.mode()
+  if mode == 'v' or mode == 'V' or mode == '' then
+    -- if we are in visual mode use the live position
+    _, csrow, cscol, _ = unpack(vim.fn.getpos('.'))
+    _, cerow, cecol, _ = unpack(vim.fn.getpos('v'))
+    if mode == 'V' then
+      -- visual line doesn't provide columns
+      cscol, cecol = 0, 999
+    end
+    -- exit visual mode
+    -- vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+  else
+    -- otherwise, use the last known visual position
+    _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
+    _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
+  end
+  -- swap vars if needed
+  if cerow < csrow then
+    csrow, cerow = cerow, csrow
+  end
+  if cecol < cscol then
+    cscol, cecol = cecol, cscol
+  end
+  local lines = vim.fn.getline(csrow, cerow)
+  -- local n = cerow-csrow+1
+  local n = M.tbl_length(lines)
+  if n <= 0 then
+    return ''
+  end
+  lines[n] = string.sub(lines[n], 1, cecol)
+  lines[1] = string.sub(lines[1], cscol)
+  return table.concat(lines, '\n')
 end
 
 return M
