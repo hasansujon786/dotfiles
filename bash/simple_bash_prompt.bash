473d052797409da5223606e7071eb92cc23706e9
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2022-05-17 16:12:18 +0600

    bash: add git status

--- bash/simple_bash_prompt.bash
+++ bash/simple_bash_prompt.bash
@@ -11,15 +11,35 @@ git-branch-name() {
   git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3-
 }
 
-dir-and-git-branch() {
+git_status() {
+  local RED="\033[0;31m"
+  local GREEN="\033[0;32m"
+  local NOCOLOR="\033[0m"
+  local YELLOW="\033[0;33m"
+  local BLACK="\033[0;30m"
+
+  local git_modified_symbol=""
+  local git_status=$(git status 2>/dev/null | grep "Your branch is ahead" 2>/dev/null)
+  if [ "$git_status" != "" ]; then git_modified_symbol="\[${YELLOW}\][!]"; fi
+
+  local git_status=$(git status --porcelain 2>/dev/null)
+  if [ "$git_status" != "" ]; then git_modified_symbol="\[${RED}\][?]"; fi
+  if [ $git_modified_symbol ]; then echo $git_modified_symbol; fi
+}
+
+git-branch() {
+  local YELLOW="\033[0;33m"
   local branch=`git-branch-name`
-  printf "\[\033[0;36m\]\w "
-  if [ $branch ]; then printf "\[\033[1;33m\] %s " $branch; fi
+  if [ $branch ]; then printf "\[${YELLOW}\] %s %s" $branch `git_status`; fi
 }
 
 re-prompt() {
   set_win_title
-  PS1="\n\[\033[35m\][\T∣\d] $(dir-and-git-branch)\n \`if [ \$? = 0 ]; then echo \[\e[0m\]; else echo \[\e[31m\]; fi\`\[\033[0m\] "
+  local PINK="\033[35m"
+  local BLUE="\033[0;36m"
+  local NOCOLOR="\033[0m"
+
+  PS1="\n\[${PINK}\][\T∣\d] \[${BLUE}\]\w $(git-branch)\n \`if [ \$? = 0 ]; then echo \[\e[0m\]; else echo \[\e[31m\]; fi\`\[${NOCOLOR}\] "
 }
 
 if [ "$color_prompt" = yes ]; then
