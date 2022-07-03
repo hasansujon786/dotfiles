78ebd9394228d00d02ddc8055ac1f7b2688e0ff5
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2022-07-03 22:25:04 +0600

    update .fzf.sh

--- bash/.fzf.sh
+++ bash/.fzf.sh
@@ -21,7 +21,7 @@ __fzf_z__() {
 }
 
 # ALT-j - cd into the selected directory
-bind -m emacs-standard '"\ej": " \C-l\C-b\C-k \C-u`__fzf_z__`\e\C-e\er\C-m\C-y\C-h\e \C-y\ey\C-x\C-x\C-d"'
+bind -m emacs-standard '"\ej": " \C-b\C-k \C-u`__fzf_z__`\e\C-e\er\C-m\C-y\C-h\e \C-y\ey\C-x\C-x\C-d"'
 
 __fzf_select__() {
   local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
