adf31de291520b6606242a51c8cad831b0703b30
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2022-07-04 03:56:25 +0600

    plug: update neoscroll

--- nvim/lua/config/neoscroll.lua
+++ nvim/lua/config/neoscroll.lua
@@ -1,14 +1,19 @@
+-- use { 'declancm/cinnamon.nvim', config = function() require('config.cinnamon') end }
 require('neoscroll').setup()
 local map = {}
 
-map['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '80' } }
-map['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '80' } }
-map['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '250' } }
-map['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250' } }
+local ease = 'circular'
+map['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '80', ease } }
+map['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '80', ease } }
+map['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250', ease } }
+map['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '250', ease } }
 map['<C-y>'] = { 'scroll', { '-0.10', 'false', '30' } }
 map['<C-e>'] = { 'scroll', { '0.10', 'false', '30' } }
 map['zt'] = { 'zt', { '30' } }
 map['zz'] = { 'zz', { '150' } }
 map['zb'] = { 'zb', { '30' } }
+
+keymap({ 'n', 'v' }, '<ScrollWheelUp>', '<C-y><C-y>')
+keymap({ 'n', 'v' }, '<ScrollWheelDown>', '<C-e><C-e>')
 
 require('neoscroll.config').set_mappings(map)
