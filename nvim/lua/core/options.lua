3b098c249bd3701dc20098918656ad28edcfc384
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2022-12-09 03:47:03 +0600

    ui: change font

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -78,7 +78,7 @@ vim.opt.fillchars = {
 }
 
 o.belloff = 'all' --                             Just turn the dang bell off
-o.guifont = 'OperatorMonoLig Nerd Font:h16'
+o.guifont = 'FiraCode NF:h12:w6' --              OperatorMonoLig Nerd Font:h16
 o.guicursor =
   'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
 o.title = true
