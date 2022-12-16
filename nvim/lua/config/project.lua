e155ab00141439848f343a2c8dec409fc6cbfbd2
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2022-12-17 02:33:30 +0600

    fix: update project_nvim

--- nvim/lua/config/project.lua
+++ nvim/lua/config/project.lua
@@ -1,2 +1,5 @@
-require('project_nvim').setup({ exclude_dirs = { 'c:' } })
+require('project_nvim').setup({
+  detection_methods = { 'pattern' },
+  exclude_dirs = { 'c:', 'c:/Users/hasan/dotfiles/nvim/.vsnip' },
+})
 -- require('telescope').load_extension('projects')
