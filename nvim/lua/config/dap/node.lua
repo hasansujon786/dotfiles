4f7543362fb6e3ea5d7122cfdea0f981084bb25b
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2022-12-22 01:38:44 +0600

    refactor: Organize node adapter config

--- nvim/lua/config/dap/node.lua
+++ nvim/lua/config/dap/node.lua
@@ -11,74 +11,47 @@ return {
       args = { dap_adapter_path .. '\\vscode-chrome-debug\\out\\src\\chromeDebug.js' }, -- C:\Users\hasan\AppData\Local\nvim-data/dap_adapters/vscode-chrome-debug/out/src/chromeDebug.js
     }
 
-    dap.configurations.javascript = {
-      {
-        name = 'Launch javascript/node2',
-        type = 'node2',
-        request = 'launch',
-        program = '${file}',
-        cwd = vim.fn.getcwd(),
-        sourceMaps = true,
-        protocol = 'inspector',
-        console = 'integratedTerminal',
-      },
-      {
-        name = 'Attach to javascript/node2',
-        type = 'node2',
-        request = 'attach',
-        cwd = vim.fn.getcwd(),
-        sourceMaps = true,
-        skipFiles = { '<node_internals>/**/*.js' },
-        protocol = 'inspector',
-      },
-      -- {
-      --   -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-      --   name = 'Attach to process with javascript/node2',
-      --   type = 'node2',
-      --   request = 'attach',
+    -- INFO: For this to work you need to make sure
+    -- Chrome is on port 9222 & the website is open (remote-debugging-port=9222 -- "%1")
+    local chrome_attach = {
+      name = 'Attach (Chrome)',
+      type = 'chrome',
+      request = 'attach',
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      program = '${file}',
+      protocol = 'inspector',
+      port = 9222,
+      webRoot = '${workspaceFolder}',
+      skipFiles = { '<node_internals>/**/*.js' },
+    }
+
+    local node_launch = {
+      name = 'Launch (node2)',
+      type = 'node2',
+      request = 'launch',
+      program = '${file}',
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      protocol = 'inspector',
+      console = 'integratedTerminal',
+    }
+    -- INFO: For this to work you need to make sure
+    -- the node process is started with the `--inspect` flag.
+    local node_attach = {
+      name = 'Attach (node2)',
+      type = 'node2',
+      request = 'attach',
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      skipFiles = { '<node_internals>/**/*.js' },
+      protocol = 'inspector',
       --   processId = require('dap.utils').pick_process,
-      -- },
-      {
-        name = 'Attach Chrome',
-        type = 'chrome',
-        request = 'attach',
-        program = '${file}',
-        cwd = vim.fn.getcwd(),
-        sourceMaps = true,
-        protocol = 'inspector',
-        port = 9222,
-        webRoot = '${workspaceFolder}',
-      },
     }
 
-    dap.configurations.javascriptreact = { -- change this to javascript if needed
-      {
-        name = 'Launch javascriptreact/chrome',
-        type = 'chrome',
-        request = 'attach',
-        cwd = vim.fn.getcwd(),
-        sourceMaps = true,
-        program = '${file}',
-        protocol = 'inspector',
-        port = 9222,
-        webRoot = '${workspaceFolder}',
-        skipFiles = { '<node_internals>/**/*.js' },
-      },
-    }
-
-    dap.configurations.typescriptreact = { -- change to typescript if needed
-      {
-        name = 'Launch typescriptreact/chrome',
-        type = 'chrome',
-        request = 'attach',
-        cwd = vim.fn.getcwd(),
-        sourceMaps = true,
-        protocol = 'inspector',
-        webRoot = '${workspaceFolder}',
-        program = '${file}',
-        port = 9222,
-        skipFiles = { '<node_internals>/**/*.js' },
-      },
-    }
+    dap.configurations.javascript = { chrome_attach, node_launch, node_attach }
+    dap.configurations.typescript = { chrome_attach, node_launch, node_attach }
+    dap.configurations.javascriptreact = { chrome_attach }
+    dap.configurations.typescriptreact = { chrome_attach }
   end,
 }
