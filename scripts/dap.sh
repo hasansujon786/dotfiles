c93abd4aae1ae2982f24cc8624f28971ec29b41d
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2023-01-09 04:08:17 +0600

    feat(dap): update install script for vscode-js-debug

--- scripts/dap.sh
+++ scripts/dap.sh
@@ -8,16 +8,25 @@ git clone https://github.com/Dart-Code/Dart-Code
 cd Dart-Code
 npx webpack --mode production
 
-# node
-cd C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters
-git clone https://github.com/microsoft/vscode-node-debug2
-cd vscode-node-debug2
-npm install
-NODE_OPTIONS=--no-experimental-fetch npm run build
+# vscode-js-debug
+cd C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters\\
+export VSCODE_JS_VER="1.72.0"
+curl -O -J -L https://codeload.github.com/microsoft/vscode-js-debug/zip/refs/tags/v${VSCODE_JS_VER}
+unzip vscode-js-debug-${VSCODE_JS_VER}.zip
+mv vscode-js-debug-${VSCODE_JS_VER} vscode-js-debug && cd vscode-js-debug && npm install --legacy-peer-deps && npm run compile
 
-# chrome javascript
-cd C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters
-git clone https://github.com/Microsoft/vscode-chrome-debug
-cd vscode-chrome-debug
-npm install
-npm run build
+
+
+# ========= depricated =========#
+# # node
+# cd C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters
+# git clone https://github.com/microsoft/vscode-node-debug2
+# cd vscode-node-debug2
+# npm install
+# NODE_OPTIONS=--no-experimental-fetch npm run build
+# # chrome javascript
+# cd C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters
+# git clone https://github.com/Microsoft/vscode-chrome-debug
+# cd vscode-chrome-debug
+# npm install
+# npm run build
