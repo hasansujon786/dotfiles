4d159cb9321b7ec2c2f2d9d3c4de410cdee1da3f
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2023-01-10 01:24:59 +0600

    feat(dap): update dart installation & remove deprecated code

--- scripts/dap.sh
+++ scripts/dap.sh
@@ -4,9 +4,11 @@ mkdir -p C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters\\
 
 # dart
 cd C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters\\
-git clone https://github.com/Dart-Code/Dart-Code
-cd Dart-Code
-npx webpack --mode production
+export DART_CODE_VER="3.56.0"
+curl -O -J -L https://codeload.github.com/Dart-Code/Dart-Code/zip/refs/tags/v${DART_CODE_VER}
+unzip Dart-Code-${DART_CODE_VER}.zip
+mv Dart-Code-${DART_CODE_VER} Dart-Code && cd Dart-Code && npm install && npm run build
+cd .. && rm Dart-Code-${DART_CODE_VER}.zip
 
 # vscode-js-debug
 cd C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters\\
@@ -14,19 +16,6 @@ export VSCODE_JS_VER="1.72.0"
 curl -O -J -L https://codeload.github.com/microsoft/vscode-js-debug/zip/refs/tags/v${VSCODE_JS_VER}
 unzip vscode-js-debug-${VSCODE_JS_VER}.zip
 mv vscode-js-debug-${VSCODE_JS_VER} vscode-js-debug && cd vscode-js-debug && npm install --legacy-peer-deps && npm run compile
+cd .. && rm vscode-js-debug-${VSCODE_JS_VER}.zip
 
 
-
-# ========= depricated =========#
-# # node
-# cd C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters
-# git clone https://github.com/microsoft/vscode-node-debug2
-# cd vscode-node-debug2
-# npm install
-# NODE_OPTIONS=--no-experimental-fetch npm run build
-# # chrome javascript
-# cd C:\\Users\\hasan\\AppData\\Local\\nvim-data\\dap_adapters
-# git clone https://github.com/Microsoft/vscode-chrome-debug
-# cd vscode-chrome-debug
-# npm install
-# npm run build
