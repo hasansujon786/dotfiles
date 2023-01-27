05dda2409aaffded1880e4041fb9f7a581aa9b3c
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2023-01-27 12:18:25 +0600

    feat(ui): improve better toggle conceal_html_class

--- nvim/lua/hasan/utils/ts_query.lua
+++ nvim/lua/hasan/utils/ts_query.lua
@@ -3,7 +3,15 @@ local namespace = vim.api.nvim_create_namespace('class_conceal')
 local group = vim.api.nvim_create_augroup('class_conceal', { clear = true })
 local M = {}
 
-local conceal_html_class = function(bufnr)
+local function del_buffer_extmark()
+  if vim.b.class_conceal_ns_ids ~= nil then
+    for _, id in pairs(vim.b.class_conceal_ns_ids) do
+      vim.api.nvim_buf_del_extmark(0, namespace, id)
+    end
+  end
+end
+
+local function conceal_html_class(bufnr)
   local language_tree = vim.treesitter.get_parser(bufnr, 'html')
   local syntax_tree = language_tree:parse()
   local root = syntax_tree[1]:root()
@@ -21,36 +29,52 @@ local conceal_html_class = function(bufnr)
     ]]
   )
 
+  del_buffer_extmark()
+  local ns_ids = {}
+
   for _, captures, metadata in query:iter_matches(root, bufnr, root:start(), root:end_()) do
     local start_row, start_col, end_row, end_col = captures[2]:range()
-    vim.api.nvim_buf_set_extmark(bufnr, namespace, start_row, start_col, {
+    local ns_id = vim.api.nvim_buf_set_extmark(bufnr, namespace, start_row, start_col, {
       end_line = end_row,
       end_col = end_col,
       conceal = metadata[2].conceal,
     })
+    table.insert(ns_ids, ns_id)
   end
+  vim.b.class_conceal_ns_ids = ns_ids
 end
 
 M.setup_tailwindcss = function(bufnr)
+  if vim.b.class_conceal_active == false then
+    return
+  end
+
   vim.wo.conceallevel = 2
+  vim.b.class_conceal_active = true
   conceal_html_class(bufnr)
 
-  vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'TextChanged', 'InsertLeave' }, {
+  vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave' }, {
     group = group,
     buffer = bufnr,
     -- pattern = '*.html',
     callback = function()
-      conceal_html_class(bufnr)
+      if vim.b.class_conceal_active then
+        conceal_html_class(bufnr)
+      end
     end,
   })
 end
 
 M.toggle_conceallevel = function()
-  if vim.wo.conceallevel == 0 then
+  if not vim.b.class_conceal_active then
+    vim.b.class_conceal_active = true
     M.setup_tailwindcss(0)
   else
+    del_buffer_extmark()
+
     vim.wo.conceallevel = 0
-    -- vim.api.nvim_del_augroup_by_id(group)
+    vim.b.class_conceal_ns_ids = nil
+    vim.b.class_conceal_active = false
   end
 end
 
