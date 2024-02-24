e20db9704fce3cd6cda16768c2fa7dd875316398
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2024-02-25 03:43:21 +0600

    feat(harpoon): mod default select & restore cursor properly

--- nvim/lua/config/mics/harpoon.lua
+++ nvim/lua/config/mics/harpoon.lua
@@ -23,6 +23,8 @@ return {
   end,
   config = function()
     local harpoon = require('harpoon')
+    local Extensions = require('harpoon.extensions')
+    local Logger = require('harpoon.logger')
 
     -- REQUIRED
     harpoon:setup({
@@ -32,6 +34,45 @@ return {
         -- key = function()
         --   return vim.loop.cwd()
         -- end,
+      },
+      default = {
+        select = function(list_item, list, options)
+          Logger:log('config_default#select', list_item, list.name, options)
+          options = options or {}
+          if list_item == nil then
+            return
+          end
+
+          local bufnr = vim.fn.bufnr(list_item.value)
+          local set_position = false
+          if bufnr == -1 then
+            set_position = true
+            bufnr = vim.fn.bufnr(list_item.value, true)
+          end
+          if not vim.api.nvim_buf_is_loaded(bufnr) then
+            vim.fn.bufload(bufnr)
+            vim.api.nvim_set_option_value('buflisted', true, {
+              buf = bufnr,
+            })
+          end
+
+          if options.vsplit then
+            vim.cmd('vsplit')
+          elseif options.split then
+            vim.cmd('split')
+          elseif options.tabedit then
+            vim.cmd('tabedit')
+          end
+
+          vim.api.nvim_set_current_buf(bufnr)
+
+          if set_position then
+            -- vim.api.nvim_win_set_cursor(0, { list_item.context.row or 1, list_item.context.col or 0, })
+            require('hasan.utils.win').restore_cussor_pos()
+          end
+
+          Extensions.extensions:emit(Extensions.event_names.NAVIGATE, { buffer = bufnr })
+        end,
       },
     })
     local maps = {
