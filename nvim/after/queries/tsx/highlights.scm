f04394a4a680e70f6a6b799649b521f25c10558c
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2024-06-07 20:13:15 +0600

    fix(ts): use separate file for tsx query highlights

---
+++ nvim/after/queries/tsx/highlights.scm
@@ -0,0 +1,6 @@
+;; extends
+; class conceal
+(jsx_attribute
+  (property_identifier) @att_name (#eq? @att_name "className")
+  (string
+  (string_fragment) @class_value) (#set! @class_value conceal "…"))
