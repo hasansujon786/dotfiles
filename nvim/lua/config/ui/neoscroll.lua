79770aee998791dbddba039c9fce7f89cc2f4b9d
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2024-07-14 01:45:29 +0600

    feat(neoscroll): update to new api

--- nvim/lua/config/ui/neoscroll.lua
+++ nvim/lua/config/ui/neoscroll.lua
@@ -1,30 +1,43 @@
-local nx = { 'n', 'x' }
+local modes = { 'n', 'x', 'v' }
 return {
   'karb94/neoscroll.nvim',
-  commit = 'e78657719485c5663b88e5d96ffcfb6a2fe3eec0',
+  -- commit = 'e78657719485c5663b88e5d96ffcfb6a2fe3eec0',
   lazy = true,
   event = 'WinScrolled',
-  opts = {},
   keys = {
-    { '<C-u>', mode = nx },
-    { '<C-d>', mode = nx },
-    { 'zt', mode = nx },
-    { 'zz', mode = nx },
-    { 'zb', mode = nx },
+    { '<C-u>', mode = modes },
+    { '<C-d>', mode = modes },
+    { '<C-f>', mode = modes },
+    { '<C-b>', mode = modes },
+    { '<C-y>', mode = modes },
+    { '<C-e>', mode = modes },
+    { 'zt', mode = modes },
+    { 'zz', mode = modes },
+    { 'zb', mode = modes },
   },
   config = function()
-    require('neoscroll').setup()
-    local map = {}
-    local ease = 'circular'
-    map['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '80', ease } }
-    map['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '80', ease } }
-    map['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250', ease } }
-    map['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '250', ease } }
-    map['<C-y>'] = { 'scroll', { '-0.10', 'false', '30' } }
-    map['<C-e>'] = { 'scroll', { '0.10', 'false', '30' } }
-    map['zt'] = { 'zt', { '30' } }
-    map['zz'] = { 'zz', { '150' } }
-    map['zb'] = { 'zb', { '30' } }
-    require('neoscroll.config').set_mappings(map)
+    local neoscroll = require('neoscroll')
+    neoscroll.setup({
+      mappings = {},
+      hide_cursor = true,
+      stop_eof = true, -- Stop at <EOF> when scrolling downwards
+      respect_scrolloff = false,
+      cursor_scrolls_alone = true,
+      easing = 'circular',
+    })
+    local keymap = {
+      ['<C-u>'] = function() neoscroll.ctrl_u({ duration = 250 }) end,
+      ['<C-d>'] = function() neoscroll.ctrl_d({ duration = 250 }) end,
+      ['<C-b>'] = function() neoscroll.ctrl_b({ duration = 250 }) end,
+      ['<C-f>'] = function() neoscroll.ctrl_f({ duration = 250 }) end,
+      ['<C-y>'] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 80 }) end,
+      ['<C-e>'] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 80 }) end,
+      ['zt'] = function() neoscroll.zt({ half_win_duration = 80 }) end,
+      ['zz'] = function() neoscroll.zz({ half_win_duration = 80 }) end,
+      ['zb'] = function() neoscroll.zb({ half_win_duration = 80 }) end,
+    }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func)
+    end
   end,
 }
