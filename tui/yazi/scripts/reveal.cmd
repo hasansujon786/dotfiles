9fb0702fa31c1c686a1d6ab56b9acd29442c229f
hasansujon786 <hasansujon786@gmail.com>
2026-01-25 03:12:23 +0600

    feat(yazi): fix reveal script path

--- tui/yazi/scripts/reveal.cmd
+++ tui/yazi/scripts/reveal.cmd
@@ -4,4 +4,5 @@ REM Yazi file reveal script
 REM
 REM https://superuser.com/questions/489240/how-to-get-filename-only-without-path-in-windows-command-line
 
-explorer /select,"%~nx1"
+REM explorer /select,""%~1""
+explorer /select,%~1
